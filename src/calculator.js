b7054fc7181de0adf82a8eba4e5f5dc36b696341
Pavitchaya Phoonsap <pavitchcya.p@ku.th>
2025-12-25 17:12:11 +0700

    Node

--- src/calculator.js
+++ src/calculator.js
@@ -12,7 +12,7 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (b === 0) {
-        throw new Error("Cannot divide by zero");
+        throw new Error("Cannot divide by zero naja");
     }
     return a / b;
 }
